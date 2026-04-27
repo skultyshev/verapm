f934676575ec2c57444dd12a19e1047c67574e47
Sergey Kultyshev <sergeykultyshev@Sergeys-iMac.local>
2026-04-27 10:52:37 -0400

    Fix badge type in layout nav

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2da420e4f7796ab6"; }
+/******/ 		__webpack_require__.h = function() { return "8e9f09a5b04715d7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
